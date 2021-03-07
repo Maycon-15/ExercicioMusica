9598dbdee415a6d3d822e4e014eef4c60949b2c9
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-07 10:26:25 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace EXERCICIO_MUSICA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Artista: MC Paulin da capita");
+            Console.WriteLine("Musica: Eu achei");
+           
+            Console.WriteLine();
+           
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("EU ACHEI");
+            Console.ForegroundColor = ConsoleColor.White;
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("E logo de manhã, fumei o primeiro do dia");
+            Console.WriteLine("Dei um abraço na minha veinha");
+            Console.WriteLine("Olhei pela janela, cadê minha nave?");
+            Console.WriteLine("Os cara levaram na mixa");
+            Console.WriteLine("Tava indo pra minha correria");
+            Console.WriteLine("Quando os cara atrasaram meu corre");
+            Console.WriteLine("Cês levou meu pão de cada dia");
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("Mas, em menos de um dia, eu achei");
+            Console.WriteLine("Minha nave jogada na mata");
+            Console.WriteLine("Sem a frente, toda depenada");
+            Console.WriteLine("E ela tava nesse cemitério");
+            Console.WriteLine("Onde o GPS não ligava");
+            Console.WriteLine("No lugar que não entra polícia");
+            Console.WriteLine("Fui clicado na minha quebrada");
+            Console.WriteLine("Da mó odio de ver essas fitas");
+           
+            Console.WriteLine();
+           
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+           
+            Console.WriteLine("Minha nave jogada na mata");
+            Console.WriteLine("Sem a frente, toda depenada");
+            Console.WriteLine("Ela tava nesse cemitério");
+            Console.WriteLine("Onde o GPS não ligava");
+            Console.WriteLine("No lugar que não ebtra polícia");
+            Console.WriteLine("Fui clicado na minha quebrada");
+            Console.WriteLine("Da mó odio de ver essas fitas");
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Eu achei");
+            Console.ForegroundColor = ConsoleColor.White;
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("E logo de manhã, fumei o primeiro do dia");
+            Console.WriteLine("Dei um abraço na minha veinha");
+            Console.WriteLine("Olhei pela janela, cadê minha nave?");
+            Console.WriteLine("Os cara levaram na mixa");
+            Console.WriteLine("Tava indo pra minha correria");
+            Console.WriteLine("Quando os cara atrasaram meu corre");
+            Console.WriteLine("Cês levou meu pão de cada dia");
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("Mas, em menos de um dia, eu achei");
+            Console.WriteLine("Minha nave jogada na mata");
+            Console.WriteLine("Sem a frente, toda depenada");
+            Console.WriteLine("E ela tava nesse cemitério");
+            Console.WriteLine("Onde o GPS não ligava");
+            Console.WriteLine("No lugar que não entra polícia");
+            Console.WriteLine("Fui clicado na minha quebrada");
+            Console.WriteLine("Da mó odio de ver essas fitas");
+           
+            Console.WriteLine();
+           
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+           
+            Console.WriteLine("Minha nave jogada na mata");
+            Console.WriteLine("Sem a frente, toda depenada");
+            Console.WriteLine("Ela tava nesse cemitério");
+            Console.WriteLine("Onde o GPS não ligava");
+            Console.WriteLine("No lugar que não ebtra polícia");
+            Console.WriteLine("Fui clicado na minha quebrada");
+            Console.WriteLine("Da mó odio de ver essas fitas");
+           
+            Console.WriteLine();
+           
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Mas, graças a Deus, eu achei");
+            Console.WriteLine("Eu achei, eu achei");
+            Console.WriteLine("Eu achei");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
